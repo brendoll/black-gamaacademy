4e2d5ec4a8ad88b3205617dc2d97d939cff95ab7
Hyago Brendoll <brendollhyago@gmail.com>
2021-07-20 23:08:52 -0300

    Limpando campos após o envio

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
-const form = document.getElementById('formRegister')
+const form = document.getElementById('formRegister');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
-    let data = {name, email}
+
+    let data = {name, email};
     let convertData = JSON.stringify(data);
 
     localStorage.setItem('lead', convertData);
+    alert(`Olá ${name} seu interesse foi registrado`);
+    form.name.value = '';
+    form.email.value = '';
 })
